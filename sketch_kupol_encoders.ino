1f0ac223763ba33a3475f2ca51d1292e56d5b896
Surin Ivan <mordw1n@mail.ru>
2024-01-15 10:54:26 +0300

    new branch

--- sketch_kupol_encoders.ino
+++ sketch_kupol_encoders.ino
@@ -2,21 +2,31 @@
 EncButton enc2(14, 15); // (4, 5) a0a1 14, 15 (свободные a0 a1 a3 d6 d7 d8)
 EncButton enc1(6, 7); // (2, 3) d6d7 6, 7
 
-int Dec = 4800; //(4800тэ в 360°) начальное 90° (1200тэ = 90°, 13,3 ТЭ = 1°, 1тэ = 0,075°)
-int Ra = 0;  // pLST начальное 1440 минут в 24 часах (1200тэ = 90° = 6час, 3,33 ТЭ = 1 мин)
+int Dec = 4800; //(4800тэ в 360°) начальное 90°      (1200тэ = 90°, 13,3 ТЭ = 1°)
+int Ra = 0;  // pLST начальное 1440 минут в 24 часах (1200тэ = 90° = 6час, 3,33333 ТЭ = 1 мин)
 float h; float m;
-
+int x = 1;
+int hRa_plus; int hRa_minus;
+int Ra_turn;
+int Ra_last;
+int pLST;
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
+  encoders();
+
+  if (Ra != Ra_last) {
+    calculation_Ra_Dec();
+  }
+}
+
+void encoders() {
   enc1.tick();
   enc2.tick();
-
   if (enc1.turn()) {
-    //  ОДНА СТОРОНА +6, ДРУГАЯ -12
     if (enc1.right()) {
       Dec -= 1;
       if (Dec < -4799) {
@@ -29,11 +39,7 @@ void loop() {
         Dec = -4799;
       }
     }
-    Serial.print("Dec: "); Serial.println(Dec);
-    //Serial.print("Dec мин: ");Serial.println((90.0 - ((4800.0 - abs(Dec)) / 13.3)) *  60);
-    //Serial.print("Dec гр: ");Serial.println(90.0 - ((4800.0 - abs(Dec)) / 13.3));
   }
-
   if (enc2.turn()) {
     if (enc2.left()) {
       Ra += 1;
@@ -47,22 +53,41 @@ void loop() {
         Ra += 1;
       }
     }
-    // Вывод
-    m = (1440.0 - (Ra / 3.33333));  //  (-pLST)  (!!!!!!!!!)
-    h = m / 60;
-
-    // ЕСЛИ:
-    // Ra > 6ч:
-    //     Dec < 0, то Ra (18ч-Ra+pLST)
-    //     Dec > 0, то Ra ( 6ч-Ra+pLST)
-    //
-    // Ra < 6ч:
-    //     Dec > 0, то Ra ( 18ч+Ra+pLST)
-    //     Dec < 0, то Ra ( 6ч+Ra+pLST)
-
-    //Serial.print("Ra: "); Serial.println(Ra);
-    Serial.print("Ra мин: "); Serial.println(int(m)%1440);
-    Serial.print("Ra час: "); Serial.print(int(h) % 24); Serial.print(","); Serial.println(int(int(h*100) %100 * 0.6) );
-    Serial.println(" ");
   }
+}
+
+void calculation_Ra_Dec() {
+  hRa_plus = (x * 360 + pLST + Ra) % 1440;
+  hRa_minus = (x * 360 + pLST - Ra) % 1440;
+
+    if (Ra > 0) {
+      if (Dec < 0) {
+        x = 3;
+        Ra_turn = hRa_minus;
+      }
+      if (Dec > 0) {
+        Ra_turn = hRa_minus;
+      }
+    }
+    if (Ra < 0) {
+      if (Dec > 0) {
+        x = 3;
+        Ra_turn = hRa_plus;
+      }
+      if (Dec < 0) {
+        Ra_turn = hRa_plus;
+      }
+    }
+
+    Ra_last = Ra;
+    m = (1440.0 - (Ra_turn / 3.33333));
+
+  // ВЫВОД
+  //Serial.print("Dec: "); Serial.println(Dec);
+  //Serial.print("Dec мин: ");Serial.println((90.0 - ((4800.0 - abs(Dec)) / 13.3)) *  60);
+  Serial.print("Dec гр: ");Serial.println(90.0 - ((4800.0 - abs(Dec)) / 13.3));
+
+  //Serial.print("Ra: "); Serial.println(Ra);
+  //Serial.print("Ra мин: "); Serial.println(int(m)%1440);
+  Serial.print("Ra час: "); Serial.print(int(m/60)%24); Serial.print(":"); Serial.println(int(m)%100*0.6);
 }
