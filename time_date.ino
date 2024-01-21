6c170727bca2f62f1d2600d933f3fa672f16f77d
Surin Ivan <mordw1n@mail.ru>
2024-01-21 19:12:50 +0300

    time fix

--- time_date.ino
+++ time_date.ino
@@ -3,12 +3,13 @@ MicroDS3231 rtc;
 
 void setup() {
   Serial.begin(9600);
+  rtc.setTime(COMPILE_TIME);
 
   // проверка наличия модуля на линии i2c
-  if (!rtc.begin()) {
-    Serial.println("DS3231 not found");
-    for(;;);
-  }
+//   if (!rtc.begin()) {
+//     Serial.println("DS3231 not found");
+//     for(;;);
+//   }
 
   // получаем все данные в структуру
   DateTime now = rtc.getTime();
@@ -35,34 +36,34 @@ void loop() {
 void printTime() {
   // получаем все данные в структуру и используем их
   // этот способ быстрее и "легче" вызова отдельных get-функций
-  DateTime now = rtc.getTime();
-
-  Serial.print(now.hour);
-  Serial.print(":");
-  Serial.print(now.minute);
-  Serial.print(":");
-  Serial.print(now.second);
-  Serial.print(" ");
-  Serial.print(now.day);
-  Serial.print(" ");
-  Serial.print(now.date);
-  Serial.print("/");
-  Serial.print(now.month);
-  Serial.print("/");
-  Serial.println(now.year);
+//   DateTime now = rtc.getTime();
+//
+//   Serial.print(now.hour);
+//   Serial.print(":");
+//   Serial.print(now.minute);
+//   Serial.print(":");
+//   Serial.print(now.second);
+//   Serial.print(" ");
+//   Serial.print(now.day);
+//   Serial.print(" ");
+//   Serial.print(now.date);
+//   Serial.print("/");
+//   Serial.print(now.month);
+//   Serial.print("/");
+//   Serial.println(now.year);
 
     // получение и вывод каждой компоненты
-//   Serial.print(rtc.getHours());
-//   Serial.print(":");
-//   Serial.print(rtc.getMinutes());
-//   Serial.print(":");
-//   Serial.print(rtc.getSeconds());
-//   Serial.print(" ");
-//   Serial.print(rtc.getDay());
-//   Serial.print(" ");
-//   Serial.print(rtc.getDate());
-//   Serial.print("/");
-//   Serial.print(rtc.getMonth());
-//   Serial.print("/");
-//   Serial.println(rtc.getYear());
+  Serial.print(rtc.getHours());
+  Serial.print(":");
+  Serial.print(rtc.getMinutes());
+  Serial.print(":");
+  Serial.print(rtc.getSeconds());
+  Serial.print(" ");
+  Serial.print(rtc.getDay());
+  Serial.print(" ");
+  Serial.print(rtc.getDate());
+  Serial.print("/");
+  Serial.print(rtc.getMonth());
+  Serial.print("/");
+  Serial.println(rtc.getYear());
 }
